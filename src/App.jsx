1dca243031395ee6868525c38f210f93749ab6a5
SalmanDoria <salmandoria@gmail.com>
2022-12-05 17:35:04 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+
+function App() {
+  const [sneakers, setSneakers] = useState([])
+
+  
+
+
+useEffect (() => {
+  const request = async () => {
+    let req = await fetch('http://localhost:3000/sneakers')
+    let res = await req.json()
+    setSneakers(res)
+  }
+  request()
+},[])
+  return (
+    <>
+    {/* <h2>Sneakers</h2> */}
+    {
+      sneakers.map((s) => (
+        <div key={s.id} className="App">
+          <h2>{s.name}</h2>
+          <img src={s.imgSrc} />
+        </div>
+      ))
+    }
+
+    </>
+  )
+}
+
+export default App
